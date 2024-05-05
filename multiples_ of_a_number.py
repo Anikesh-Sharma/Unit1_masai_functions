5f2fccf5f307706e0df8f5ba418633144bdef271
Anikesh Sharma <91007286+Anikesh-Sharma@users.noreply.github.com>
2024-05-05 18:15:43 +0530

    Create multiples_ of_a_number.py

---
+++ multiples_ of_a_number.py
@@ -0,0 +1,12 @@
+# List Multiples of a Number
+
+# Write a function named listMultiples that takes two numbers n and limit. The function should print the first n multiples of limit.
+
+# E.g., listMultiples(3, 5) should print 3, 6, 9, 12, 15
+
+
+def listMultiples(n, limit):
+    for i in range(1, limit+1):
+        print((n*i), end = ' ')
+
+n_mult_limit = listMultiples(3, 5)
